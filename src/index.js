b97d8dc257c486e152b1df9dd3abbaecb3a7b9e2
zoeharris16 <zoe.harris16@gmail.com>
2025-05-25 17:04:44 +0100

    Added a link back to home page

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ function updateCity(event) {
           <div class="time">${cityTime.format(
             "h:mm:ss [<small>]A[</small>]"
           )}</div>
-        </div>`;
+        </div> <a href="/">All Cities</a>`;
 }
 
 updateTime();
