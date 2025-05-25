35c47896c3188d35020e1338ede0cb70e70577cf
zoeharris16 <zoe.harris16@gmail.com>
2025-05-25 16:55:48 +0100

    Added functionality to choose current location

--- src/index.js
+++ src/index.js
@@ -34,6 +34,9 @@ function updateTime() {
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityTime = moment().tz(cityTimeZone);
   let cityName = cityTimeZone.split("/")[1];
   let cityContainerElement = document.querySelector("#city-container");
