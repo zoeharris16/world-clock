d99c0c11fb9d5937c18e4ae555faf078b3bbf649
zoeharris16 <zoe.harris16@gmail.com>
2025-05-25 15:53:14 +0100

    coded current date and time for home page cities using moment for js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+let londonElement = document.querySelector("#london");
+let londonDateElement = londonElement.querySelector(".date");
+let londonTimeElement = londonElement.querySelector(".time");
+let londonTime = moment().tz("Europe/London");
+londonDateElement.innerHTML = londonTime.format("D MMMM YYYY");
+londonTimeElement.innerHTML = londonTime.format("h:mm [<small>]A[</small>]");
+
+let parisElement = document.querySelector("#paris");
+let parisDateElement = parisElement.querySelector(".date");
+let parisTimeElement = parisElement.querySelector(".time");
+let parisTime = moment().tz("Europe/Paris");
+parisDateElement.innerHTML = moment().format("D MMMM YYYY");
+parisTimeElement.innerHTML = parisTime.format("h:mm [<small>]A[</small>]");
+
+let newYorkElement = document.querySelector("#new-york");
+let newYorkDateElement = newYorkElement.querySelector(".date");
+let newYorkTimeElement = newYorkElement.querySelector(".time");
+let newYorkTime = moment().tz("America/New_York");
+newYorkDateElement.innerHTML = moment().format("D MMMM YYYY");
+newYorkTimeElement.innerHTML = newYorkTime.format("h:mm [<small>]A[</small>]");
